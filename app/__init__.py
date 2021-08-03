a669aad76e4675d31c92963f5a47a9563dde5878
Ting-Chien <eric821210@gmail.com>
2021-08-04 00:23:39 +0800

    db connection

--- app/__init__.py
+++ app/__init__.py
@@ -3,40 +3,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from app.db import get_db
 
 def create_app():
 
     # build app configuration
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SECRET_KEY'] = 'dev'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'db.sqlite3')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    db = SQLAlchemy()
-
-    class User(db.Model):
-        __tablename__ = 'user'
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(30), unique=True, nullable=False)
-        password = db.Column(db.String(30), nullable=False)
-
-        db_user_post = db.relationship("Post", backref="post")
-
-        def __init__(self, username, password):
-            self.username = username
-            self.password = password
-
-    class Post(db.Model):
-        __tablenme__ = 'post'
-        id = db.Column(db.Integer, primary_key=True)
-        author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-        created = db.Column(db.DateTime, default=datetime.now, nullable=False)
-        title = db.Column(db.String(50), nullable=False)
-        body = db.Column(db.String(255), nullable=False)
-
-        def __init__(self, author_id, title, body):
-            self.author_id = author_id
-            self.title = title
-            self.body = body
 
     # ensure the instance folder exists
     try:
@@ -49,12 +24,13 @@ def create_app():
         return 'Hello World!'
 
     # init db
+    from . import db
     db.init_app(app)
-    # create db connect
+    # test db connection
     @app.route('/create_db')
-    def createt_db():
-        db.create_all()
-        return 'ok'
+    def create_db():
+        get_db()
+        return 'Create db'
 
     # auth routes
     from app.views import auth
--- app/views/auth.py
+++ app/views/auth.py
@@ -1,14 +1,41 @@
 from flask import (
     Blueprint,
     render_template,
+    request,
+    redirect,
+    url_for,
+    flash
 )
+from app.db import get_db, User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif User.query.filter_by(username=username).first() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            print('add a user')
+            user = User(username, password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
     return render_template('auth/register.html')
 
-@bp.route('login', methods=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     return render_template('auth/login.html')
